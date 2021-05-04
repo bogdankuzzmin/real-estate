b4e1cc201b6cb5efeacc628318b6538de14bb062
Bogdan <bogdankuzzmin@gmail.com>
2021-05-04 23:22:30 +0600

    added sortTypes as constant

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+export const APARTMENT_COUNT_PER_STEP = 10;
+
+export const SortType = {
+  PRICE_UP: 'price-up',
+  PRICE_DOWN: 'price-down',
+  NEWEST: 'newest',
+  SQUARE: 'square',
+  ROOMS: 'rooms',
+};
--- src/utils/sortApartments.js
+++ src/utils/sortApartments.js
@@ -1,21 +1,23 @@
 import moment from "moment";
 
+import {SortType} from "../constants/constants";
+
 const sort = (apartments, sortType) => {
   switch (sortType) {
-    case 'price-up':
+    case SortType.PRICE_UP:
       return apartments.sort((a, b) =>  b.price - a.price);
-    case 'price-down':
+    case SortType.PRICE_DOWN:
       return apartments.sort((a, b) => a.price - b.price);
-    case 'newest':
+    case SortType.NEWEST:
       return apartments.sort((a, b) => {
         const dateA = moment(a.date);
         const dateB = moment(b.date);
 
         return dateB - dateA;
       });
-    case 'square':
+    case SortType.SQUARE:
       return apartments.sort((a, b) =>  b.square - a.square);
-    case 'rooms':
+    case SortType.ROOMS:
       return apartments.sort((a, b) =>  b.rooms - a.rooms);
 
     default: return apartments;
