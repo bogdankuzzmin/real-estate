54b4b17e681228f0bec46b4180a1f6558c2c987c
Bogdan <bogdankuzzmin@gmail.com>
2021-05-05 00:36:35 +0600

    added ApartmentSorting component

---
+++ src/components/ApartmentSorting/ApartmentSoritng.js
@@ -0,0 +1,33 @@
+import {useDispatch, useSelector} from 'react-redux';
+
+import {sortApartments} from '../../store/actions/apartment';
+
+import classes from './ApartmentSoritng.module.scss';
+
+const ApartmentSorting = props => {
+  const dispatch = useDispatch();
+
+  const sortApartmentsH = (sortType) => dispatch(sortApartments(sortType));
+  const sortType = useSelector(state => state.sortType);
+
+  const changeSortTypeHandler = (event) => {
+    sortApartmentsH(event.target.value);
+  };
+
+  return (
+    <div className={classes.Sorting}>
+      <div className={classes.SortWrapper}>
+        <label htmlFor="sortingApartments">Sort: </label>
+        <select value={sortType} id="sortingApartments" onChange={(event) => changeSortTypeHandler(event)}>
+          <option value="price-up">Price (High to Low)</option>
+          <option value="price-down">Price (Low to High)</option>
+          <option value="newest">Newest</option>
+          <option value="square">Square Feet</option>
+          <option value="rooms">Rooms</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ApartmentSorting;
