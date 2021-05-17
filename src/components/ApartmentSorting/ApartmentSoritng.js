77995b5a498d5b84d02877404ccce082fe76b7ae
Bogdan <bogdankuzzmin@gmail.com>
2021-05-17 23:49:20 +0600

    [ApartmentSorting - code refactoring.

--- src/components/ApartmentSorting/ApartmentSoritng.js
+++ src/components/ApartmentSorting/ApartmentSoritng.js
@@ -1,29 +1,30 @@
 import {useDispatch, useSelector} from 'react-redux';
 
+import {SortType} from "../../constants/constants";
 import {sortApartments} from '../../store/actions/apartment';
 
 import classes from './ApartmentSoritng.module.scss';
 
-
 const ApartmentSorting = props => {
   const dispatch = useDispatch();
 
-  const sortApartmentsH = (sortType) => dispatch(sortApartments(sortType));
+  const sortApartmentsHandler = (sortType) => dispatch(sortApartments(sortType));
   const sortType = useSelector(state => state.sortType);
 
   const changeSortTypeHandler = (event) => {
-    sortApartmentsH(event.target.value);
+    sortApartmentsHandler(event.target.value);
   };
 
   return (
     <form className={classes.Sorting}>
       <label htmlFor="sortingApartments">Sort: </label>
-      <select value={sortType} id="sortingApartments" onChange={(event) => changeSortTypeHandler(event)}>
-        <option value="price-up">Price (High to Low)</option>
-        <option value="price-down">Price (Low to High)</option>
-        <option value="newest">Newest</option>
-        <option value="square">Square Feet</option>
-        <option value="rooms">Rooms</option>
+      <select value={sortType} id="sortingApartments"
+              onChange={(event) => changeSortTypeHandler(event)}>
+        <option value={SortType.PRICE_UP}>Price (High to Low)</option>
+        <option value={SortType.PRICE_DOWN}>Price (Low to High)</option>
+        <option value={SortType.NEWEST}>Newest</option>
+        <option value={SortType.SQUARE}>Square Feet</option>
+        <option value={SortType.ROOMS}>Rooms</option>
       </select>
     </form>
   );
