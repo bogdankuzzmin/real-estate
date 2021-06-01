6239f39a2139e78c78452b63cbe032174de5ba32
Bogdan <bogdankuzzmin@gmail.com>
2021-06-01 20:05:54 +0600

    [Layout] - added logic for closing and opening SideDrawer.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,20 +1,34 @@
-import {Fragment} from 'react';
+import {Fragment, useState} from 'react';
 
 import classes from './Layout.module.scss';
 
 import Toolbar from '../../components/Navigation/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer';
 import Footer from '../../components/Footer';
 
-const layout = props => {
+const Layout = props => {
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
+
+  const sideDrawerCloseHandler = () => {
+    setShowSideDrawer(false);
+  };
+
+  const sideDrawerToggleHandler = () => {
+    setShowSideDrawer(!showSideDrawer);
+  };
+
   return (
     <Fragment>
-      <Toolbar />
+      <Toolbar drawerToggle={sideDrawerToggleHandler} />
+      <SideDrawer open={showSideDrawer} close={sideDrawerCloseHandler} />
+
       <main className={classes.Main}>
         {props.children}
       </main>
+
       <Footer />
     </Fragment>
   );
 };
 
-export default layout;
+export default Layout;
