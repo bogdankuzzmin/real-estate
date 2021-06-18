da261fbff5c4b125535fe8a155c95aabd9e6607a
Bogdan <bogdankuzzmin@gmail.com>
2021-06-18 22:19:48 +0600

    [Input UI] - added styles for type input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,17 +1,25 @@
 import classes from './Input.module.scss';
 
 const input = props => {
-  const inputClasses = [classes.InputCheckbox];
+  const inputClasses = [classes.Input];
+  const wrapperClasses = [classes.InputWrapper];
+
+  if (props.disableTopMargin) {
+    wrapperClasses.push(classes.DisableTopMargin);
+  }
 
   let customCheckbox;
   if (props.type === 'checkbox') {
     customCheckbox = <span className={classes.CustomCheckbox}>Custom Checkbox</span>;
+    inputClasses.push(classes.InputCheckbox);
+    wrapperClasses.push(classes.InputCheckboxWrapper);
   }
 
   return (
-    <p className={classes.InputWrapper}>
+    <p className={wrapperClasses.join(' ')}>
       <label htmlFor={props.value}>
-        <input className={inputClasses.join(' ')} {...props.input} />
+        <input className={inputClasses.join(' ')}
+               onChange={props.changed} {...props.input} />
         {customCheckbox}
 
         {props.label}
